13ae1bd2783904eae4d6afcaa634825ffd2fd5bf
sumita kumari <sumitakumari904@gmail.com>
2024-07-05 21:52:47 +0530

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: sumita kumari <sumitakumari904@gmail.com>

---
+++ Day2/Lab008.py
@@ -0,0 +1,8 @@
+x=int(input("enter a number :"))
+y=int(input("enter a number :"))
+if x>y:
+ print("greater no. is :",x)
+else:
+ print("greatest no is:",y)
+
+"""Print the largest of 4"""
---
+++ Day2/Lab009.py
@@ -0,0 +1,10 @@
+#Assign single value to multiple variables
+a=b=c=3
+print(a)
+print(b)
+print(c)
+#Assign multiple value to multiple variables in single line
+a,b,c=1,2,3
+print(a)
+print(b)
+print(c)
