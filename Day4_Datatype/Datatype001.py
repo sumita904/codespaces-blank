0adfb732a78f613f8174f59e6c503ac6b9761247
sumita kumari <sumitakumari904@gmail.com>
2024-07-06 20:50:18 +0530

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: sumita kumari <sumitakumari904@gmail.com>

---
+++ Day4_Datatype/Datatype001.py
@@ -0,0 +1,91 @@
+#---String Datatype---
+x= "Hello World"
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---int Datatype---
+x=20
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---int Datatype---
+x=20.5
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---complex Datatype---
+x=1j
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---list Datatype---
+x=["cake","choco","latte"]
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---tuple Datatype---
+x=("cake","choco","latte")
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---range Datatype---
+x=range(6)
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---dict Datatype---
+x={"Britania" : "cake", "Price" : 10}
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---set Datatype---
+x={"cake","choco","latte"}
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---frozenset Datatype---
+x= frozenset({"cake","choco","latte"})
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---boolean Datatype---
+x=True
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---bytes Datatype---
+x= b"Hello"
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---bytearray Datatype---
+x= bytearray(5)
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---memoryview Datatype---
+x= memoryview(bytes(5))
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---NoneType Datatype---
+x= None
+print(x)
+#display the data type of x :
+print(type(x))
+
+
---
+++ Day4_Datatype/SettingSpecificDatatype.py
@@ -0,0 +1,87 @@
+#---String Datatype---
+x= str("Hello World")
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---int Datatype---
+x=int(20)
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---float Datatype---
+x=float(20.5)
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---complex Datatype---
+x=complex(1j)
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---list Datatype---
+x=list(["cake","choco","latte"])
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---tuple Datatype---
+x=tuple(("cake","choco","latte"))
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---range Datatype---
+x=range(6)
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---dict Datatype---
+x=dict({"Britania" : "cake", "Price" : 10})
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---set Datatype---
+x=set({"cake","choco","latte"})
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---frozenset Datatype---
+x= frozenset({"cake","choco","latte"})
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---boolean Datatype---
+x=bool(5)
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---bytes Datatype---
+x= bytes(5)
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---bytearray Datatype---
+x= bytearray(5)
+print(x)
+#display the data type of x :
+print(type(x))
+
+#---memoryview Datatype---
+x= memoryview(bytes(5))
+print(x)
+#display the data type of x :
+print(type(x))
+
+
+
+
