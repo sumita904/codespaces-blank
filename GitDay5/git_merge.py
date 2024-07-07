af7331df6d6a789c7ea93882e7806ccaee7f991b
Sumita Kumari <122207179+sumita904@users.noreply.github.com>
2024-07-07 23:24:35 +0530

    Update git_merge.py

--- GitDay5/git_merge.py
+++ GitDay5/git_merge.py
@@ -12,4 +12,4 @@ print(my_string[4:6:1])
 # now write in terminal git commit -m "developerA story complete"
 #now write in terminal- git checkout master
 #now delete developerA branch --- git branch -d developerA
-#now push to repository
+#now push to repository using --- git push origin master
