f3d124d8a10695cfe6be3470955f38b6698c8f38
Sumita Kumari <sumitakumari904@gmail.com>
2024-07-07 23:01:36 +0530

    developerA story complete

---
+++ GitDay5/git_merge.py
@@ -0,0 +1,10 @@
+#use command - git branch (new branch name for creation)
+# git checkout branch---------- it means switching to other branch
+#now write the code
+#Slicing with a Step --slice step cannot be zero
+#The default step is 1.Python 3 also allows us to slice a string by defining a step through which we can skip characters in the string.
+
+my_string= "This is python programming"
+print(my_string[0:2])
+print(my_string[3:7:1])
+print(my_string[4:6:1])
