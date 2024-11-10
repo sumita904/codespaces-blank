31cfb925c540c705cf3218a53793ab3a650367c2
sumita kumari <sumitakumari904@gmail.com>
2024-11-10 12:00:05 +0530

    cat photogallery

--- Html-CSS/project.js
+++ Html-CSS/project.js
@@ -42,10 +42,52 @@
 // console.log(num++);
 // console.log(newNum);
 
-var a=30;
-var b=10;
+// var a=30;
+// var b=10;
 
-console.log(a!=b);
-console.log(a>b || b<0 || b>0); //use command+ D for removing and adding duplicate codes
-console.log(!(a>b || b<a));  //Logical NOT ! takes Truth to Falsity (it's called logical omplement,negation)
-console.log(!true);
+// console.log(a!=b);
+// console.log(a>b || b<0 || b>0); //use command+ D for removing and adding duplicate codes
+// console.log(!(a>b || b<a));  //Logical NOT ! takes Truth to Falsity (it's called logical omplement,negation)
+// console.log(!true);
+
+// var a = true+true+true*3;
+// console.log(a);
+
+// var a=10;
+// var b=20;
+// var c=b;
+// b=a;
+// a=c;
+// console.log("value of a is:" + a);
+// console.log("value of b is:" + b);
+
+// var a=10;
+// var b='10';
+
+// console.log(a==b);
+// console.log(typeof(b));
+// console.log(typeof(a===b));
+
+// var tomr='rain';
+
+// if (tomr=='rain'){
+//     console.log('take a raincoat');
+// }else{
+//     console.log('donot take a raincoat');
+// }
+
+var year=1000;
+debugger;
+if(year% 4==0){
+    if(year%100==0){
+        if(year%400==0){
+        console.log("The year "+ year +"is a leap year");
+        }else{
+            console.log("The year "+ year +"is not a leap year");
+        }
+    }else{
+        console.log("The year "+ year +"is a leap year");
+    }
+}else{
+    console.log("The year "+ year +"is not a leap year");
+}
